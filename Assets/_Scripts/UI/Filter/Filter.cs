7a0d3f2a98671cd50e0070ffb66ffeccb70b3599
Chuẩn <chuan.nguyenvn@hcmut.edu.vn>
2023-09-07 10:35:12 +0700

    feat: filter button

---
+++ Assets/_Scripts/UI/Filter/Filter.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine.Scripting;
+using UnityEngine.UIElements;
+
+namespace UI.Filter
+{
+    public class Filter : VisualElement
+    {
+        #region UXML
+
+        [Preserve]
+        public new class UxmlFactory : UxmlFactory<Filter, UxmlTraits>
+        {
+        }
+
+        [Preserve]
+        public new class UxmlTraits : VisualElement.UxmlTraits
+        {
+        }
+
+        #endregion
+
+        private Image _filterIcon;
+        private TextElement _text;
+
+        public Filter()
+        {
+            AddToClassList("filter");
+
+            _filterIcon = new Image { name = "FilterIcon" };
+            _filterIcon.AddToClassList("icon");
+            _filterIcon.AddToClassList("dark-icon");
+            Add(_filterIcon);
+
+            _text = new TextElement { name = "Text", text = "Filter" };
+            Add(_text);
+        }
+    }
+}
