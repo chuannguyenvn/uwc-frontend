0f5f74b4a15f28a47262892260f160696c5d8586
Chuẩn <chuan.nguyenvn@hcmut.edu.vn>
2023-10-02 17:52:20 +0700

    feat: perfect worker list

---
+++ Assets/_Scripts/UI/Views/Messaging/MessagingView.cs
@@ -0,0 +1,14 @@
+﻿using UI.Base;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+namespace UI.Views.Messaging
+{
+    public class MessagingView : View
+    {
+        public MessagingView() : base(nameof(MessagingView), true)
+        {
+            styleSheets.Add(Resources.Load<StyleSheet>("Stylesheets/UI/Views/Messaging/MessagingView"));
+        }
+    }
+}
--- Assets/_Scripts/UI/Views/Workers/WorkerListEntry.cs
+++ Assets/_Scripts/UI/Views/Workers/WorkerListEntry.cs
@@ -7,25 +7,32 @@ namespace UI.Views.Workers
     public class WorkerListEntry : AdaptiveElement
     {
         private Image _image;
+
+        private VisualElement _textContainer;
         private TextElement _nameText;
         private TextElement _statusText;
 
         public WorkerListEntry(UserProfile workerProfile) : base(nameof(WorkerListEntry))
         {
-            _image = new Image { name = "Image" };
+            AddToClassList("list-entry");
+
+            _image = new Image { name = "Avatar" };
             Add(_image);
 
-            _nameText = new TextElement { name = "PrimaryText" };
+            _textContainer = new VisualElement { name = "TextContainer" };
+            Add(_textContainer);
+
+            _nameText = new TextElement { name = "NameText" };
             _nameText.AddToClassList("normal-text");
             _nameText.AddToClassList("black-text");
             _nameText.text = workerProfile.FirstName + " " + workerProfile.LastName;
-            Add(_nameText);
+            _textContainer.Add(_nameText);
 
-            _statusText = new TextElement { name = "SecondaryText" };
-            _nameText.AddToClassList("sub-text");
-            _nameText.AddToClassList("grey-text");
+            _statusText = new TextElement { name = "StatusText" };
+            _statusText.AddToClassList("sub-text");
+            _statusText.AddToClassList("grey-text");
             _statusText.text = workerProfile.Address;
-            Add(_statusText);
+            _textContainer.Add(_statusText);
         }
     }
 }
