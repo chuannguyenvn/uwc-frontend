7b93641357edc6fe69f0b0cf7ee13c4efde97d4e
Chuẩn <chuan.nguyenvn@hcmut.edu.vn>
2023-10-02 23:33:50 +0700

    feat: emptying logs panel

--- Assets/_Scripts/UI/Views/Tasks/Details/EmptyingLogPanel.cs
+++ Assets/_Scripts/UI/Views/Tasks/Details/EmptyingLogPanel.cs
@@ -6,10 +6,28 @@ namespace UI.Views.Tasks.Details
 {
     public class EmptyingLogPanel : Panel
     {
+        public TextElement TitleText;
+
         public EmptyingLogPanel() : base(nameof(EmptyingLogPanel))
         {
             styleSheets.Add(Resources.Load<StyleSheet>("Stylesheets/Views/Tasks/Details/EmptyingLogPanel"));
             AddToClassList("rounded-32px");
+
+            TitleText = new TextElement { name = "TitleText" };
+            TitleText.AddToClassList("grey-text");
+            TitleText.AddToClassList("sub-text");
+            TitleText.text = "Emptying logs";
+            Add(TitleText);
+
+            for (int i = 0; i < 10; i++)
+            {
+                var child = new TextElement();
+                child.name = "EmptyLog";
+                child.text = "Robert Sampletext Jr. - 11:56 AM";
+                child.AddToClassList("black-text");
+                child.AddToClassList("normal-text");
+                Add(child);
+            }
         }
     }
 }
