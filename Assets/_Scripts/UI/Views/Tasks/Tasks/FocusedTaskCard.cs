95b5bc571037adf3eaa8d4a0128354eecdf8939d
Chuẩn <chuan.nguyenvn@hcmut.edu.vn>
2023-10-03 10:47:37 +0700

    feat: focused task shadow

--- Assets/_Scripts/UI/Views/Tasks/Tasks/FocusedTaskCard.cs
+++ Assets/_Scripts/UI/Views/Tasks/Tasks/FocusedTaskCard.cs
@@ -9,58 +9,114 @@ namespace UI.Views.Tasks.Tasks
     public class FocusedTaskCard : View
     {
         public VisualElement ContentContainer;
+        public VisualElement Panel;
+        public VisualElement Mask;
+
         public VisualElement AddressContainer;
         public TextElement AddressText;
+
         public VisualElement DetailsContainer;
-        public TextElement CurrentLoadText;
-        public TextElement EtaText;
+        public VisualElement CurrentLoadContainer;
+        public TextElement CurrentLoadTitleText;
+        public TextElement CurrentLoadValueText;
+        public VisualElement EtaContainer;
+        public TextElement EtaTitleText;
+        public TextElement EtaValueText;
 
         public FocusedTaskCard(McpFillStatus mcpFillStatus) : base(nameof(FocusedTaskCard))
         {
             styleSheets.Add(Resources.Load<StyleSheet>("Stylesheets/Views/Tasks/Tasks/FocusedTaskCard"));
             AddToClassList("task-card");
 
-            ContentContainer = new VisualElement { name = "ContentContainer" };
-            Add(ContentContainer);
-
-            AddressContainer = new VisualElement { name = "AddressContainer" };
-            ContentContainer.Add(AddressContainer);
-
-            AddressText = new TextElement { name = "AddressText" };
-            AddressText.text = "Address";
-            AddressText.AddToClassList("normal-text");
-            AddressText.AddToClassList("white-text");
-            AddressContainer.Add(AddressText);
-
-            DetailsContainer = new VisualElement { name = "DetailsContainer" };
-            ContentContainer.Add(DetailsContainer);
-
-            CurrentLoadText = new TextElement { name = "CurrentLoadText" };
-            CurrentLoadText.text = "69%";
-            AddressText.AddToClassList("normal-text");
-            AddressText.AddToClassList("black-text");
-            DetailsContainer.Add(CurrentLoadText);
-
-            EtaText = new TextElement { name = "EtaText" };
-            EtaText.text = "ETA: 4:20";
-            AddressText.AddToClassList("normal-text");
-            AddressText.AddToClassList("black-text");
-            DetailsContainer.Add(EtaText);
+            CreateMask();
+            CreateAddress();
+            CreateDetails();
 
             switch (mcpFillStatus)
             {
                 case McpFillStatus.Full:
-                    ContentContainer.AddToClassList("full");
+                    AddressContainer.AddToClassList("full");
                     break;
                 case McpFillStatus.AlmostFull:
-                    ContentContainer.AddToClassList("almost-full");
+                    AddressContainer.AddToClassList("almost-full");
                     break;
                 case McpFillStatus.NotFull:
-                    ContentContainer.AddToClassList("not-full");
+                    AddressContainer.AddToClassList("not-full");
                     break;
                 default:
                     throw new ArgumentOutOfRangeException(nameof(mcpFillStatus), mcpFillStatus, null);
             }
+        }
+
+        private void CreateMask()
+        {
+            ContentContainer = new VisualElement { name = "ContentContainer" };
+            Add(ContentContainer);
+
+            Panel = new VisualElement { name = "Panel" };
+            ContentContainer.Add(Panel);
+
+            Mask = new VisualElement { name = "Mask" };
+            ContentContainer.Add(Mask);
+        }
+
+        private void CreateAddress()
+        {
+            AddressContainer = new VisualElement { name = "AddressContainer" };
+            AddressContainer.AddToClassList("sub-container");
+            Mask.Add(AddressContainer);
+
+            AddressText = new TextElement { name = "AddressText" };
+            AddressText.text = "Address";
+            AddressText.AddToClassList("title-text");
+            AddressText.AddToClassList("white-text");
+            AddressContainer.Add(AddressText);
+        }
+
+        private void CreateDetails()
+        {
+            DetailsContainer = new VisualElement { name = "DetailsContainer" };
+            DetailsContainer.AddToClassList("sub-container");
+            Mask.Add(DetailsContainer);
+
+            CreateCurrentLoad();
+            CreateEta();
+        }
+
+        private void CreateCurrentLoad()
+        {
+            CurrentLoadContainer = new VisualElement { name = "CurrentLoadContainer" };
+            DetailsContainer.Add(CurrentLoadContainer);
+            
+            CurrentLoadTitleText = new TextElement { name = "CurrentLoadTitleText" };
+            CurrentLoadTitleText.text = "Current load:";
+            CurrentLoadTitleText.AddToClassList("normal-text");
+            CurrentLoadTitleText.AddToClassList("black-text");
+            CurrentLoadContainer.Add(CurrentLoadTitleText);
+            
+            CurrentLoadValueText = new TextElement { name = "CurrentLoadText" };
+            CurrentLoadValueText.text = "69%";
+            CurrentLoadValueText.AddToClassList("title-text");
+            CurrentLoadValueText.AddToClassList("black-text");
+            CurrentLoadContainer.Add(CurrentLoadValueText);
+        }
+
+        private void CreateEta()
+        {
+            EtaContainer = new VisualElement { name = "EtaContainer" };
+            DetailsContainer.Add(EtaContainer);
+            
+            EtaTitleText = new TextElement { name = "EtaTitleText" };
+            EtaTitleText.text = "ETA:";
+            EtaTitleText.AddToClassList("normal-text");
+            EtaTitleText.AddToClassList("black-text");
+            EtaContainer.Add(EtaTitleText);
+            
+            EtaValueText = new TextElement { name = "EtaValueText" };
+            EtaValueText.text = "4:20";
+            EtaValueText.AddToClassList("title-text");
+            EtaValueText.AddToClassList("black-text");
+            EtaContainer.Add(EtaValueText);
         }
     }
 }
