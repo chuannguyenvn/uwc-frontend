ef031b210d309be800be0ca144d297fd1f412ae0
Chuẩn <chuan.nguyenvn@hcmut.edu.vn>
2023-10-30 15:16:32 +0700

    feat: base report view

---
+++ Assets/_Scripts/UI/Views/Reports/ReportingView.cs
@@ -0,0 +1,15 @@
+﻿using UI.Base;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+namespace UI.Views.Reports
+{
+    public class ReportingView : View
+    {
+        public ReportingView() : base(nameof(ReportingView))
+        {
+            styleSheets.Add(Resources.Load<StyleSheet>("Stylesheets/Views/Reports/ReportingView"));
+            AddToClassList("full-view");
+        }
+    }
+}
