ad5d635162a495e76f06ccc88c962a180a9a84fe
Chuẩn <chuan.nguyenvn@hcmut.edu.vn>
2023-10-31 16:22:24 +0700

    feat: labels

--- Assets/_Scripts/UI/Views/Reports/Cards/GraphCard.cs
+++ Assets/_Scripts/UI/Views/Reports/Cards/GraphCard.cs
@@ -13,11 +13,21 @@ namespace UI.Views.Reports.Cards
         private List<float> _totalMcpFillLevelValues;
         private List<DateTime> _mcpEmptiedTimestamps;
 
+        private List<TextElement> _totalMcpFillLevelLabels;
+        private List<TextElement> _mcpEmptiedLabels;
+
         public GraphCard() : base(nameof(GraphCard))
         {
             styleSheets.Add(Resources.Load<StyleSheet>("Stylesheets/Views/Reports/Cards/GraphCard"));
             GenerateMockData();
+            CreateGraphValues();
             generateVisualContent += GenerateVisualContent;
+            RegisterCallback<GeometryChangedEvent>(OnGeometryChanged);
+        }
+
+        private void OnGeometryChanged(GeometryChangedEvent evt)
+        {
+            ModifyGraphValues();
         }
 
         private void GenerateMockData()
@@ -45,12 +55,40 @@ namespace UI.Views.Reports.Cards
             _mcpEmptiedTimestamps = response.McpEmptiedTimestamps;
         }
 
+        private void CreateGraphValues()
+        {
+            _totalMcpFillLevelLabels = new List<TextElement>();
+            _mcpEmptiedLabels = new List<TextElement>();
+
+
+            for (var i = 0; i < 8; i++)
+            {
+                var mcpFillLevelLabel = new TextElement
+                {
+                    text = $"{i}%",
+                    name = "McpFillLevelLabel"
+                };
+                _totalMcpFillLevelLabels.Add(mcpFillLevelLabel);
+                Add(mcpFillLevelLabel);
+
+
+                var mcpEmptiedLabel = new TextElement
+                {
+                    text = $"{i}",
+                    name = "McpEmptiedLabel"
+                };
+                _mcpEmptiedLabels.Add(mcpEmptiedLabel);
+                Add(mcpEmptiedLabel);
+                mcpEmptiedLabel.style.position = Position.Absolute;
+            }
+        }
+
         private void GenerateVisualContent(MeshGenerationContext ctx)
         {
             var painter = ctx.painter2D;
-            Debug.Log("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA");
+
             DrawGraphLines(painter);
-            CreateGraphValues(painter);
+            ModifyGraphValues();
             DrawMcpFillLevelGraph(painter);
             DrawMcpEmptiedGraph(painter);
             CreateTimestamps(painter);
@@ -80,8 +118,34 @@ namespace UI.Views.Reports.Cards
             }
         }
 
-        private void CreateGraphValues(Painter2D painter)
+        private void ModifyGraphValues()
         {
+            var leftPadding = 64;
+            var rightPadding = 64;
+            var topPadding = 64;
+            var bottomPadding = 128;
+            var startX = leftPadding;
+            var endX = resolvedStyle.width - rightPadding;
+            var graphHeight = resolvedStyle.height - (topPadding + bottomPadding);
+
+            for (var i = 0; i < 8; i++)
+            {
+                var size = new Vector2(50, 20);
+
+                var mcpFillLevelLabelPosition = new Vector2(startX, graphHeight / 7 * i + topPadding);
+                var mcpFillLevelLabel = _totalMcpFillLevelLabels[7 - i];
+                mcpFillLevelLabel.style.left = mcpFillLevelLabelPosition.x - size.x / 2;
+                mcpFillLevelLabel.style.right = resolvedStyle.width - (mcpFillLevelLabelPosition.x + size.x / 2);
+                mcpFillLevelLabel.style.top = mcpFillLevelLabelPosition.y - size.y / 2;
+                mcpFillLevelLabel.style.bottom = resolvedStyle.height - (mcpFillLevelLabelPosition.y + size.y / 2);
+
+                var mcpEmptiedLabel = _mcpEmptiedLabels[7 - i];
+                var mcpEmptiedLabelPosition = new Vector2(endX, graphHeight / 7 * i + topPadding);
+                mcpEmptiedLabel.style.left = mcpEmptiedLabelPosition.x - size.x / 2;
+                mcpEmptiedLabel.style.right = resolvedStyle.width - (mcpEmptiedLabelPosition.x + size.x / 2);
+                mcpEmptiedLabel.style.top = mcpEmptiedLabelPosition.y - size.y / 2;
+                mcpEmptiedLabel.style.bottom = resolvedStyle.height - (mcpEmptiedLabelPosition.y + size.y / 2);
+            }
         }
 
         private void DrawMcpFillLevelGraph(Painter2D painter)
