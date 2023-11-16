48d9d198b06f2a0fd7a3972dea67d42986eb867c
Chuan <chuan.nguyenvn@hcmut.edu.vn>
2023-11-17 00:57:48 +0700

    feat: add tasks icon

--- Assets/_Scripts/UI/Views/Tasks/TasksView.cs
+++ Assets/_Scripts/UI/Views/Tasks/TasksView.cs
@@ -14,10 +14,13 @@ namespace UI.Views.Tasks
         public TaskList TaskList;
         public TaskDetails TaskDetails;
         public AssignTaskFlow AssignTaskFlow;
+        public VisualElement AssignTaskButton;
+        public VisualElement AssignTaskButtonIcon;
 
         public TasksView() : base(nameof(TasksView))
         {
             styleSheets.Add(Resources.Load<StyleSheet>("Stylesheets/Views/Tasks/TasksView"));
+            AddToClassList("tasks-view");
 
             if (Configs.IS_DESKTOP)
             {
@@ -26,8 +29,17 @@ namespace UI.Views.Tasks
                 TaskList = new TaskList();
                 Add(TaskList);
 
-                // AssignTaskFlow = new AssignTaskFlow();
-                // Add(AssignTaskFlow);
+                AssignTaskFlow = new AssignTaskFlow();
+                AssignTaskFlow.style.display = DisplayStyle.None;
+                Add(AssignTaskFlow);
+
+                AssignTaskButton = new Button() { name = "AssignTaskButton" };
+                Add(AssignTaskButton);
+
+                AssignTaskButtonIcon = new VisualElement() { name = "AssignTaskButtonIcon" };
+                AssignTaskButton.Add(AssignTaskButtonIcon);
+
+                AssignTaskButton.RegisterCallback<ClickEvent>(evt => AssignTaskFlow.style.display = DisplayStyle.Flex);
             }
             else
             {
@@ -52,6 +64,10 @@ namespace UI.Views.Tasks
         {
             if (Configs.IS_DESKTOP) DataStoreManager.Tasks.AllTaskList.Unfocus();
             else DataStoreManager.Tasks.PersonalTaskList.Unfocus();
+        }
+
+        public new class UxmlFactory : UxmlFactory<TasksView, UxmlTraits>
+        {
         }
     }
 }
