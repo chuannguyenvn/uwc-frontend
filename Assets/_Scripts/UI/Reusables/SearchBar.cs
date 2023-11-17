6fc6ca98c70ee5d820519885f25645eb08fec32b
Chuan <chuan.nguyenvn@hcmut.edu.vn>
2023-11-17 10:07:59 +0700

    refactor: search bar

--- Assets/_Scripts/UI/Reusables/SearchBar.cs
+++ Assets/_Scripts/UI/Reusables/SearchBar.cs
@@ -8,6 +8,7 @@ namespace UI.Reusables
     public class SearchBar : AdaptiveElement
     {
         private readonly Action<string> _searched;
+        
         private TextField _searchField;
         private Button _searchButton;
         private VisualElement _searchIcon;
@@ -15,25 +16,32 @@ namespace UI.Reusables
         public SearchBar(Action<string> searched) : base(nameof(SearchBar))
         {
             _searched = searched;
+
             styleSheets.Add(Resources.Load<StyleSheet>("Stylesheets/Reusables/SearchBar"));
             AddToClassList("search-bar");
 
+            CreateSearchField();
+            CreateSearchIcon();
+        }
+
+        private void CreateSearchField()
+        {
             _searchField = new TextField { name = "SearchField" };
             _searchField.AddToClassList("normal-text");
             _searchField.AddToClassList("black-text");
             _searchField.textEdition.placeholder = "Search...";
             Add(_searchField);
 
+            _searchField.RegisterCallback<ChangeEvent<string>>(e => Search());
+        }
+
+        private void CreateSearchIcon()
+        {
             _searchButton = new Button { name = "SearchButton" };
             _searchField.Add(_searchButton);
 
             _searchIcon = new VisualElement { name = "SearchIcon" };
             _searchButton.Add(_searchIcon);
-
-            _searchField.RegisterCallback<ChangeEvent<string>>(e =>
-            {
-                Search();
-            });
         }
 
         private void Search()
