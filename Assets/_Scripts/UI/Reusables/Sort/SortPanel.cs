63b0309c8b2fbf93d45f070aa1345a7a5efecd59
Chuan <chuan.nguyenvn@hcmut.edu.vn>
2023-11-20 22:01:56 +0700

    feat: sort title

--- Assets/_Scripts/UI/Reusables/Sort/SortPanel.cs
+++ Assets/_Scripts/UI/Reusables/Sort/SortPanel.cs
@@ -1,17 +1,31 @@
 ﻿using System;
 using System.Collections.Generic;
 using UI.Base;
+using UnityEngine.UIElements;
 
 namespace UI.Reusables.Sort
 {
     public class SortPanel : AdaptiveElement
     {
         public List<SortType> SortStates { get; } = new List<SortType>();
+
+        private TextElement _titleText;
         private List<SortButton> _sortButtons = new();
 
         public SortPanel() : base(nameof(SortPanel))
         {
             ConfigureUss(nameof(SortPanel));
+
+            CreateTitleText();
+        }
+
+        private void CreateTitleText()
+        {
+            _titleText = new TextElement { name = "TitleText" };
+            _titleText.AddToClassList("sub-text");
+            _titleText.AddToClassList("grey-text");
+            _titleText.text = "Sort by:";
+            Add(_titleText);
         }
 
         public void CreateSortButton(string name, Action sortCallback)
