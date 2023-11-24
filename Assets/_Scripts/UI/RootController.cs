ac6bbeafb2a5d3a883113200efa55f930f7f18b1
Chuẩn <chuan.nguyenvn@hcmut.edu.vn>
2023-11-24 15:54:11 +0700

    feat: back button clicked event in RootController

--- Assets/_Scripts/UI/RootController.cs
+++ Assets/_Scripts/UI/RootController.cs
@@ -6,11 +6,21 @@ namespace UI.Base
 {
     public class RootController : Singleton<RootController>
     {
+        public event Action BackButtonPressed;
+        
         public UIDocument RootDocument;
 
         private void Start()
         {
             Application.targetFrameRate = 60;
+        }
+
+        private void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.Escape))
+            {
+                BackButtonPressed?.Invoke();
+            }
         }
     }
 }
