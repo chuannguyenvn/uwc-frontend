5617e86fdc6c20f9734742da5feef70cf4ba48d9
Chuan <chuan.nguyenvn@hcmut.edu.vn>
2023-11-24 23:56:35 +0700

    fix: some Android related bugs

--- Assets/_Scripts/Maps/MapManager.cs
+++ Assets/_Scripts/Maps/MapManager.cs
@@ -1,10 +1,13 @@
 ﻿using UI.Base;
+using UnityEngine;
 using Utilities;
 
 namespace Maps
 {
     public class MapManager : PersistentSingleton<MapManager>
     {
+        public GameObject MapGameObject;
+
         private void Update()
         {
             OnlineMaps.instance.control.allowZoom =
