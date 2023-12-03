9592da08bfdb500105667d181f10c793537244ea
Chuan <chuan.nguyenvn@hcmut.edu.vn>
2023-12-03 15:05:38 +0700

    feat: update SharedLibrary

--- Assets/_Scripts/UI/Views/Mcps/AssignTaskProcedure/SetAssigningOptionsStep.cs
+++ Assets/_Scripts/UI/Views/Mcps/AssignTaskProcedure/SetAssigningOptionsStep.cs
@@ -1,15 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
+using Commons.Types;
 using LocalizationNS;
 using UI.Reusables.Procedure;
 using UI.Views.Settings;
+using Step = UI.Reusables.Procedure.Step;
 
 namespace UI.Views.Mcps.AssignTaskProcedure
 {
     public class SetAssigningOptionsStep : Step
     {
         private SettingList _settingList;
-        public OptimizeRoute OptimizeRoute { get; private set; } = OptimizeRoute.None;
+        public OptimizeRouting OptimizeRouting { get; private set; } = OptimizeRouting.None;
         public OptimizeAutoAssignment OptimizeAutoAssignment { get; private set; } = OptimizeAutoAssignment.TimeEfficient;
 
         public SetAssigningOptionsStep(Flow flow, int stepIndex) : base(flow, stepIndex, false,
@@ -21,12 +23,12 @@ namespace UI.Views.Mcps.AssignTaskProcedure
             AddToContainer(_settingList);
 
             _settingList.Add(new ChoiceSettingListEntry(Localization.GetSentence(Sentence.TasksView.OPTIMIZE_ROUTE),
-                () => OptimizeRoute.None.ToString(),
+                () => OptimizeRouting.None.ToString(),
                 new Dictionary<string, Action>
                 {
-                    { Sentence.TasksView.NONE, () => OptimizeRoute = OptimizeRoute.None },
-                    { Sentence.TasksView.SELECTED, () => OptimizeRoute = OptimizeRoute.Selected },
-                    { Sentence.TasksView.ALL, () => OptimizeRoute = OptimizeRoute.All },
+                    { Sentence.TasksView.NONE, () => OptimizeRouting = OptimizeRouting.None },
+                    { Sentence.TasksView.SELECTED, () => OptimizeRouting = OptimizeRouting.Selected },
+                    { Sentence.TasksView.ALL, () => OptimizeRouting = OptimizeRouting.All },
                 }));
 
             _settingList.Add(new ChoiceSettingListEntry(Localization.GetSentence(Sentence.TasksView.OPTIMIZE_AUTO_ASSIGNMENT),
@@ -47,18 +49,5 @@ namespace UI.Views.Mcps.AssignTaskProcedure
         {
             base.Reset();
         }
-    }
-
-    public enum OptimizeRoute
-    {
-        None,
-        Selected,
-        All,
-    }
-
-    public enum OptimizeAutoAssignment
-    {
-        TimeEfficient,
-        CostOptimized,
     }
 }
